bc34c4eb42fccbae3ba6b935f4fe87099a65a484
Ziang Liu <liuziang_2023@163.com>
2024-06-08 23:23:00 +0200

    change train.py

--- train.py
+++ train.py
@@ -189,6 +189,7 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     logging.info(f'Using device {device}')
 
     # Change here to adapt to your data
